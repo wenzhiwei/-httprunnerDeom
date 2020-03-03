a1e84e8833302c313fcbf2e9a7d251cd4ff638c3
wenzhiwei <wenzhiwei1984@163.com>
2020-03-03 18:29:50 +0800

    代码修改

--- tests/test_single_api.py
+++ tests/test_single_api.py
@@ -9,16 +9,18 @@ class TestSingleApi:
 
 
     def test_single_api(self):
-        single_api_yml="E:/httprunnerDeom/api/mubu_homepage.yml"
+        #single_api_yml="E:/httprunnerDeom/api/mubu_homepage.yml"
+        single_api_yml=os.path.join(os.path.dirname(__file__),"api","mubu_homepage.yml")
+        print("fielefle:"+single_api_yml)
         load= load_yaml(single_api_yml)
         assert load["request"]["url"] =="http://mubu.com/"
 
     def test_run_single_api(self):
-        single_api_yml = "E:/httprunnerDeom/api/mubu_login_submit.yml"
+        single_api_yml = os.path.join(os.path.dirname(__file__), "api", "mubu_login_submit.yml")
         result = runner_yaml(single_api_yml)
         assert result == True
 
-        single_api_yml = "E:/httprunnerDeom/api/mubu_homepage.yml"
+        single_api_yml=os.path.join(os.path.dirname(__file__),"api","mubu_homepage.yml")
         result = runner_yaml(single_api_yml)
         assert result == True
 
